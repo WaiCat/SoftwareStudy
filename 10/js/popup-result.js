d41df7b0af77abda3c8f16903b272ad75e20439f
WaiCat <rxg1123@gmail.com>
2023-06-02 21:59:09 +0900

    23.06.02

--- 10/js/popup-result.js
+++ 10/js/popup-result.js
@@ -1,8 +1,15 @@
 function openPop() {
-    var newWin = window.open("\popup-result.html", "", "width=400, height=400");
-    if(newWin == null) {
-      alert("팝업이 차단되어 있습니다. 팝업 차단을 해제하고 새로고침해 주세요.");
-    }
+  let x = localStorage.getItem('x');
+  let y = localStorage.getItem('y');
+  var newWin = window.open(
+    'popup-result.html',
+    '',
+    `width=400, height=400, left=${x}, top=${y}`
+  );
+
+  if (newWin == null) {
+    alert('팝업이 차단되어 있습니다. 팝업 차단을 해제하고 새로고침해 주세요.');
+  }
 }
-        
+
 window.onload = openPop;
